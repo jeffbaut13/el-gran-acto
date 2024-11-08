69ca8c96560ba7c73cc82204e948838e449382fc
jeffbaut13 <jeferssonabv@gmail.com>
2024-11-08 16:08:23 -0500

    ajuste de color y archivos2

--- src/components/main/SectionMain.jsx
+++ src/components/main/SectionMain.jsx
@@ -4,19 +4,29 @@ import { VideoBackground } from "../VideoBackground";
 export const SectionMain = () => {
   const { count, increase, decrease, reset } = useCounterStore();
   return (
-    <main className="w-full h-screen inline-block relative flexCenter flex-col">
+    <main className="w-full h-screen relative flex justify-between flex-col py-12">
       <VideoBackground />
-      <div className="w-1/3">
+      <div className="w-full flexCenter flex-col">
+        <div className="w-[25.5rem]">
+          <figure className="inline-block w-full">
+            <img src="/iconos/logo.svg" alt="" />
+          </figure>
+          <hr className="w-full my-6" />
+        </div>
 
-      <figure className="inline-block w-full">
-        <img src="/iconos/logo.svg" alt="" />
-      </figure>
-        <hr className="w-full my-6" />
+        <h1 className="tracking-[0.15em] text-sm">
+          <strong>EN NAVIDAD,</strong> ENTREGAR ES MEJOR QUE RECIBIR.
+        </h1>
       </div>
-
-      <h1 className="tracking-widest">
-        <strong>EN NAVIDAD,</strong> ENTREGAR ES MEJOR QUE RECIBIR.
-      </h1>
+      <div className="w-full flexCenter">
+        <figure className="w-10">
+          <img src="/iconos/play.svg" alt="" />
+        </figure>
+      </div>
+      <div className="w-full flex justify-around items-center">
+        <button>ESCUCHA YO NO ME OLVIDO DE TI</button>
+        <button>DONA TU TIEMPO A UN <br />ABUELITO EN NAVIDAD</button>
+      </div>
     </main>
   );
 };
