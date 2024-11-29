a87048f981ce6f94699ae31825c2eec4209144af
Christian Camilo Fajardo Hernandez <chfajard@gmail.com>
2024-11-29 10:35:36 -0500

    descargar

--- src/components/boleta/ticket.jsx
+++ src/components/boleta/ticket.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 const Ticket = () => {
   const { id } = useParams(); // Obtenemos el ID dinámico de la URL
@@ -32,6 +34,27 @@ const Ticket = () => {
     if (id) fetchTicketData();
   }, [id]);
 
+  const handleDownloadPDF = async () => {
+    const ticketElement = document.getElementById("ticket"); // Elemento a capturar
+    if (!ticketElement) return;
+
+    try {
+      const canvas = await html2canvas(ticketElement, {
+        scale: 2, // Mejor calidad
+      });
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("portrait", "mm", "a4");
+
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (canvas.height * pdfWidth) / canvas.width;
+
+      pdf.addImage(imgData, "PNG", 0, 0, pdfWidth, pdfHeight);
+      pdf.save("boleta.pdf"); // Nombre del archivo
+    } catch (err) {
+      console.error("Error al generar el PDF:", err);
+    }
+  };
+
   if (loading) {
     return <div>Cargando boleta...</div>;
   }
@@ -41,12 +64,15 @@ const Ticket = () => {
   }
 
   return (
-    <div className="w-full h-full relative">
+    <div id="ticket" className="w-full h-full relative">
       <img className="w-full h-full" src="/imagenes/BOLETA-SIN-QR.png" alt="Boleta base" />
-      <button className="absolute xs:w-full md:w-[23.7rem] xs:bottom-[29.5rem] md:bottom-[19.5rem] text-white border-none px-8 text-sm font-StageGroteskRegular normal-case left-1/2 transform -translate-x-1/2 rounded-xl">
-        Descarga tu boleta como PDF o toma una captura de pantalla para presentarla el día del evento
+      <button
+        onClick={handleDownloadPDF}
+        className="absolute xs:w-full md:w-[23.7rem] xs:bottom-[5.5rem] h-14 md:bottom-[19.5rem] border-none normal-case left-1/2 transform -translate-x-1/2 rounded-xl"
+      >
+        
       </button>
-      <div className="bg-white absolute xs:bottom-[10rem] md:bottom-[6rem] left-1/2 transform -translate-x-1/2 rounded-xl">
+      <div className="bg-white absolute xs:bottom-[11rem] md:bottom-[4.8rem] left-1/2 transform -translate-x-1/2 rounded-xl">
         {qrCode ? (
           <img className="xs:w-52 md:w-40 rounded-xl" src={qrCode} alt="QR Code" />
         ) : (
