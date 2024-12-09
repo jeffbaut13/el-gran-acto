abd0d004cdaa76e7d30528902c8ae3baaca423ff
Christian Camilo Fajardo Hernandez <chfajard@gmail.com>
2024-12-09 09:09:22 -0500

    rama7

--- src/components/agendar/SeleccionarFecha.jsx
+++ src/components/agendar/SeleccionarFecha.jsx
@@ -187,7 +187,7 @@ const SeleccionarFecha = ({ documentoId, tipoInteraccion }) => {
             {selectedDate && (
               <>
                 <select
-                  className="border border-primary rounded-lg p-2"
+                  className="border border-primary rounded-lg p-2 text-center"
                   value={selectedHour}
                   onChange={(e) => setSelectedHour(e.target.value)}
                 >
